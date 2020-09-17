49dd75010e47b05da1c3463afed3cf4d4df67755
JeanSOUDIER <soudier.jean@gmail.com>
2020-09-17 02:01:57 -0700

    modif prog bn_army

--- PROG/BN_robot/main.cpp
+++ PROG/BN_robot/main.cpp
@@ -7,7 +7,10 @@
 
 int main(int argc, char** argv) {
 	int lim_min[] = {0, 0, 0, 0, 0, 0}, lim_max[] = {1000, 1000, 1000, 1000, 1000, 1000};
-    Arm WidowWL(6, ttyUSB_ARBO, 115200, lim_min, lim_max);
+    Arm WidowWL(6, ttyUSB_ARBO, 115200, lim_min, lim_max, 5000);
+
+    WidowWL.SetAxePos(1, 500);
+    WidowWL.MoveArm(true);
     
     return 0; 
 }
