c3924a40352baeea796d791f22474c2363432734
JeanSOUDIER <44265577+JeannotDu67@users.noreply.github.com>
2020-09-21 08:04:37 +0200

    add squence.hpp

---
+++ PROG/BN_robot/Sequencer.hpp
@@ -0,0 +1,29 @@
+#ifndef SEQUENCER_H
+#define SEQUENCER_H
+
+#include "Arm.hpp"
+#include "Lidar.hpp"
+#include "Bluetooth.hpp"
+#include "MobileBase.hpp"
+#include "String.h"
+#include "../SequenceHandler/sequencehandler.h"
+
+constexpr int DIST_BASE = 200;
+constexpr int DIST_ARM = 50;
+constexpr int MAX_LENGTH_ARM = DIST_BASE/DIST_ARM;
+
+class Sequencer {
+	public:
+		explicit Sequencer(Arm* = nullptr, Bluetooth* = nullptr, MobileBase* = nullptr);
+		~Sequencer();
+
+		void Execute();
+	private:
+		Arm* m_WidowXL;
+		Bluetooth* m_BLE;
+		MobileBase* m_TurtleBot;
+
+		SequenceHandler seqHandler;
+};
+
+#endif //SEQUENCER_H
