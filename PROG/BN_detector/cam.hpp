513f7b902620bf80244ea771a495b06ac08391ce
JeanSOUDIER <44265577+JeannotDu67@users.noreply.github.com>
2020-11-08 14:38:48 +0100

    modif BN Cam thread imgShow

--- PROG/BN_detector/cam.hpp
+++ PROG/BN_detector/cam.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <pthread.h>
 #include "Matrix.hpp"
 
 constexpr int CV_IMWRITE_JPEG_QUALITY = 1;
@@ -23,6 +24,15 @@ class Cam {
 		void ImgShow();
 		cv::Mat ImgShow(std::string path);
 		void Test();
+
+		static void* CamHelper(void *context);
+        void StartThread();
+	protected:
+        void* ThreadRun();
+        cv::Mat ArrayToMat();
+        void MatToArray(cv::Mat img);
+        void Cam::UpdateImShow();
+
 	private:
 		std::string gstreamer_pipeline (int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method);
 		void ImageWrite(const cv::Mat &image, const std::string filename);
@@ -33,6 +43,12 @@ class Cam {
 		std::string m_pipeline;
 		bool m_start = false;
 
+		std::array<std::array<std::atomic<int> , 1280>, 720> m_buffImg;
+		std::vector<cv::Mat> m_imgShow;
+		std::atomic<bool> m_start;
+		std::atomic<bool> m_buff;
+
+		pthread_t *inc_x_thread;
 };
 
 #endif //CAM_H
